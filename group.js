db796ac397b4f870006e42ebee41f4be30a62b56
agiprmsty22-oss <agiprmsty22@gmail.com>
2026-01-17 09:04:51 +0700

    Add files via upload

---
+++ group.js
@@ -0,0 +1,245 @@
+const groups = {
+    1: {
+        foto: [
+            "images/group1.jpeg",
+            "images/group1/group1_1.jpeg",
+            "images/group1/group1_2.jpeg",
+            "images/group1/group1_3.jpeg",
+            "images/group1/group1_4.jpeg",
+            "images/group1/group1_5.jpeg",
+            "images/group1/group1_6.jpeg",
+            "images/group1/group1_7.jpeg",
+            "images/group1/group1_8.jpeg",
+            "images/group1/group1_9.jpeg",
+            "images/group1/group1_10.jpeg",
+            "images/group1/group1_11.jpeg",
+            "images/group1/group1_12.jpeg",
+            "images/group1/group1_13.jpeg",
+            "images/group1/group1_14.jpeg"
+        ]
+    },
+    2: {
+        foto: [
+            "images/group2.jpeg",
+            "images/group2/group2_1.jpeg",
+            "images/group2/group2_2.jpeg",
+            "images/group2/group2_3.jpeg",
+            "images/group2/group2_4.jpeg",
+            "images/group2/group2_5.jpeg",
+            "images/group2/group2_6.jpeg",
+            "images/group2/group2_7.jpeg",
+            "images/group2/group2_8.jpeg",
+            "images/group2/group2_9.jpeg",
+            "images/group2/group2_10.jpeg",
+            "images/group2/group2_11.jpeg",
+            "images/group2/group2_12.jpeg",
+            "images/group2/group2_13.jpeg",
+            "images/group2/group2_14.jpeg",
+            "images/group2/group2_15.jpeg",
+            "images/group2/group2_16.jpeg",
+            "images/group2/group2_17.jpeg",
+            "images/group2/group2_18.jpeg",
+            "images/group2/group2_19.jpeg",
+            "images/group2/group2_20.jpeg"
+        ]
+    },
+    3: {
+        foto: [
+            "images/group3.jpeg",
+            "images/group3/group3_1.jpeg",
+            "images/group3/group3_2.jpeg",
+            "images/group3/group3_3.jpeg",
+            "images/group3/group3_4.jpeg",
+            "images/group3/group3_5.jpeg",
+            "images/group3/group3_6.jpeg",
+            "images/group3/group3_7.jpeg",
+            "images/group3/group3_8.jpeg",
+            "images/group3/group3_9.jpeg",
+            "images/group3/group3_10.jpeg",
+            "images/group3/group3_11.jpeg",
+            "images/group3/group3_12.jpeg",
+            "images/group3/group3_13.jpeg",
+            "images/group3/group3_14.jpeg",
+            "images/group3/group3_15.jpeg",
+            "images/group3/group3_16.jpeg"
+
+        ]
+    },
+    4: {
+        foto: [
+            "images/group4.jpeg",
+            "images/group4/group4_1.jpeg",
+            "images/group4/group4_2.jpeg",
+            "images/group4/group4_3.jpeg",
+            "images/group4/group4_4.jpeg",
+            "images/group4/group4_5.jpeg",
+            "images/group4/group4_6.jpeg",
+            "images/group4/group4_7.jpeg",
+            "images/group4/group4_8.jpeg",
+            "images/group4/group4_9.jpeg",
+            "images/group4/group4_10.jpeg",
+            "images/group4/group4_11.jpeg",
+            "images/group4/group4_12.jpeg",
+            "images/group4/group4_13.jpeg",
+            "images/group4/group4_14.jpeg",
+            "images/group4/group4_15.jpeg",
+            "images/group4/group4_16.jpeg",
+            "images/group4/group4_17.jpeg",
+            "images/group4/group4_18.jpeg",
+            "images/group4/group4_19.jpeg",
+            "images/group4/group4_20.jpeg",
+            "images/group4/group4_21.jpeg",
+            "images/group4/group4_22.jpeg",
+            "images/group4/group4_23.jpeg",
+            "images/group4/group4_24.jpeg",
+            "images/group4/group4_25.jpeg",
+            "images/group4/group4_26.jpeg",
+            "images/group4/group4_27.jpeg",
+            "images/group4/group4_28.jpeg",
+            "images/group4/group4_29.jpeg",
+            "images/group4/group4_30.jpeg",
+            "images/group4/group4_31.jpeg",
+            "images/group4/group4_32.jpeg",
+            "images/group4/group4_33.jpeg",
+            "images/group4/group4_34.jpeg",
+            "images/group4/group4_35.jpeg",
+            "images/group4/group4_36.jpeg",
+            "images/group4/group4_37.jpeg",
+            "images/group4/group4_38.jpeg"
+
+        ]
+    },
+    5: {
+        foto: [
+            "images/group5.jpeg",
+            "images/group5/group5_1.jpeg",
+            "images/group5/group5_2.jpeg",
+            "images/group5/group5_3.jpeg",
+            "images/group5/group5_4.jpeg",
+            "images/group5/group5_5.jpeg",
+            "images/group5/group5_6.jpeg",
+            "images/group5/group5_7.jpeg",
+            "images/group5/group5_8.jpeg",
+            "images/group5/group5_9.jpeg",
+            "images/group5/group5_10.jpeg",
+            "images/group5/group5_11.jpeg",
+            "images/group5/group5_12.jpeg",
+            "images/group5/group5_13.jpeg",
+            "images/group5/group5_14.jpeg",
+            "images/group5/group5_15.jpeg",
+            "images/group5/group5_16.jpeg",
+            "images/group5/group5_17.jpeg",
+            "images/group5/group5_18.jpeg"
+
+        ]
+    },
+    6: {
+        foto: [
+            "images/group6.jpeg",
+            "images/group6/group6_1.jpeg",
+            "images/group6/group6_2.jpeg",
+            "images/group6/group6_3.jpeg",
+            "images/group6/group6_4.jpeg",
+            "images/group6/group6_5.jpeg",
+            "images/group6/group6_6.jpeg",
+            "images/group6/group6_7.jpeg"
+        ]
+    },
+    7: {
+        foto: [
+            "images/group7.jpeg",
+            "images/group7/group7_1.jpeg",
+            "images/group7/group7_2.jpeg",
+            "images/group7/group7_3.jpeg",
+            "images/group7/group7_4.jpeg",
+            "images/group7/group7_5.jpeg",
+            "images/group7/group7_6.jpeg",
+            "images/group7/group7_7.jpeg",
+            "images/group7/group7_8.jpeg",
+            "images/group7/group7_9.jpeg",
+            "images/group7/group7_10.jpeg",
+            "images/group7/group7_11.jpeg",
+            "images/group7/group7_12.jpeg",
+            "images/group7/group7_13.jpeg",
+            "images/group7/group7_14.jpeg",
+            "images/group7/group7_15.jpeg",
+            "images/group7/group7_16.jpeg",
+            "images/group7/group7_17.jpeg",
+            "images/group7/group7_18.jpeg",
+            "images/group7/group7_19.jpeg",
+            "images/group7/group7_20.jpeg",
+            "images/group7/group7_21.jpeg",
+            "images/group7/group7_22.jpeg",
+            "images/group7/group7_23.jpeg"
+
+        ]
+    },
+    8: {
+        foto: [
+            "images/group8.jpeg",
+            "images/group8/group8_1.jpeg",
+            "images/group8/group8_2.jpeg",
+            "images/group8/group8_3.jpeg",
+            "images/group8/group8_4.jpeg",
+            "images/group8/group8_5.jpeg",
+            "images/group8/group8_6.jpeg",
+            "images/group8/group8_7.jpeg",
+            "images/group8/group8_8.jpeg",
+            "images/group8/group8_9.jpeg",
+            "images/group8/group8_10.jpeg",
+            "images/group8/group8_11.jpeg",
+            "images/group8/group8_12.jpeg",
+            "images/group8/group8_13.jpeg",
+            "images/group8/group8_14.jpeg",
+            "images/group8/group8_15.jpeg",
+            "images/group8/group8_16.jpeg",
+            "images/group8/group8_17.jpeg"
+
+        ]
+    },
+    9: {
+        foto: [
+            "images/group9.jpeg",
+            "images/group9/group9_1.jpeg",
+            "images/group9/group9_2.jpeg",
+            "images/group9/group9_3.jpeg",
+            "images/group9/group9_4.jpeg",
+            "images/group9/group9_5.jpeg",
+            "images/group9/group9_6.jpeg",
+            "images/group9/group9_7.jpeg",
+            "images/group9/group9_8.jpeg",
+            "images/group9/group9_9.jpeg",
+            "images/group9/group9_10.jpeg",
+            "images/group9/group9_11.jpeg",
+            "images/group9/group9_12.jpeg",
+            "images/group9/group9_13.jpeg",
+            "images/group9/group9_14.jpeg",
+            "images/group9/group9_15.jpeg",
+            "images/group9/group9_16.jpeg",
+            "images/group9/group9_17.jpeg",
+            "images/group9/group9_18.jpeg",
+            "images/group9/group9_19.jpeg",
+            "images/group9/group9_20.jpeg"
+
+        ]
+        
+    },
+    10: {
+        foto: [
+            "images/group10.jpeg",
+            "images/group10/group10_1.jpeg",
+            "images/group10/group10_2.jpeg",
+            "images/group10/group10_3.jpeg",
+            "images/group10/group10_4.jpeg",
+            "images/group10/group10_5.jpeg",
+            "images/group10/group10_6.jpeg",
+            "images/group10/group10_7.jpeg",
+            "images/group10/group10_8.jpeg",
+            "images/group10/group10_9.jpeg",
+            "images/group10/group10_10.jpeg",
+            "images/group10/group10_11.jpeg",
+            "images/group10/group10_12.jpeg"
+        ]
+        
+    }
+};
